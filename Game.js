ba3a93df5620d99c25a40928a0841015c009ae9a
Irfan Suleman <ivsuleman@gmail.com>
2018-11-13 12:32:12 +0000

    added extra excitement

--- Game.js
+++ Game.js
@@ -2,7 +2,7 @@ class Game {
     static render() {
         rootEl.innerHTML = `
             <div id='grid'>I'm a grid!</div>
-            <div>I'm a list!</div>
+            <div>I'm a list!!!</div>
             <div>I keep the time. You have ${state.timeLeft} seconds left!</div>
         `
     }
