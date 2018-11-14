5bff7f91f61f67bcd3284f8bb6cc3969aeb2b21f
Amalie Smidth <amaliesmidth@gmail.com>
2018-11-14 19:09:48 +0000

    amended list-box functions and rendering. added image id tags into state16 and state32

--- Game.js
+++ Game.js
@@ -14,23 +14,27 @@ class Game {
             <h1>LOGO WILL GO HERE</h1>
             <div id="game-container">
                 <div id="photo-grid">
-                    ${Array(16).fill().map((_, idx) => `<div id='grid-box-${idx}' class="grid-box">${idx} </div>`).join('')}
+                    ${Array(16).fill().map((_, idx) => `<div id='grid-box-${idx}' class="grid-box"></div>`).join('')}
                 </div>
                 <div id="photo-list">
-                    ${Array(32).fill().map((_, idx) => `<div id='list-box-${idx}' class="list-box">${idx}</div>`).join('')}
+                    ${Array(32).fill().map((_, idx) => `<div id='list-box-${idx}' class="list-box"></div>`).join('')}
                 </div>
             </div>
             <div id='timer'>TIMER WILL GO HERE You have ${State.timeLeft} seconds left!</div>
             `
-        this.getImages(State.category).then(() => this.dataFunction())
+        this.getImages(State.category)
+            .then(() => this.gridDataFunction())
+            // .then(() => this.listDataFunction())
+            .then(() => this.renderLevel1())
     }
 
-    static dataFunction () {
+    static gridDataFunction () {
         const gridData = []
         document.querySelectorAll('.grid-box').forEach((gridBox, index) => {
             const gridBoxObj = {}
             gridBoxObj.gridBox = gridBox
             gridBoxObj.imgUrl = State.images16[index].urls.regular 
+            gridBoxObj.imgId = State.images16[index].id
             gridData.push(gridBoxObj)
         })
         State.gridData = [...gridData]
@@ -63,7 +67,6 @@ class Game {
         array.forEach(item => {
             const gridBoxObj = {...State.gridData[item]}
             const boxEl = document.querySelector(`#grid-box-${item}`)
-            console.log(item)
             boxEl.style.background = `url(${gridBoxObj.imgUrl}) no-repeat center center`
             boxEl.style.backgroundSize = 'cover'
         })
@@ -75,6 +78,8 @@ class Game {
 
         setTimeout(() => {
             this.clearLevel(imgIndexes)
+            this.listDataFunction()
+            this.renderPhotoList()
         }, 3000)
         // TODO: once image has rendered to page start count down (3 seconds) then turn image white
         // firstBoxEl.style.background = 'white'
@@ -119,16 +124,46 @@ class Game {
         array.forEach(item => {document.querySelectorAll('.grid-box')[item].style.background = 'white'})
     }
 
-    static renderPhotoList() {
-        const randomizedImagesUrls = this.shuffleImages(State.images32)
-        const listBoxes = document.querySelectorAll('.list-box')
-        listBoxes.forEach((listBox, index) => {
-            // const listBoxImage = listBoxes[index].querySelector('img')
-            // listBoxImage.src = randomizedImagesUrls[index]
-            listBoxes[index].style.background = `url(${randomizedImagesUrls[index]}) no-repeat center center`;
-            listBoxes[index].style.backgroundSize = 'cover'
+    static listDataFunction() {
+        const listData = []
+        document.querySelectorAll('.list-box').forEach((listBox, index) => {
+            const listBoxObj = {}
+            console.log(listBox)
+            listBoxObj.listBox = listBox
+            listBoxObj.imgUrl = State.images32[index].urls.regular
+            listBoxObj.imgId = State.images32[index].id
+            listData.push(listBoxObj)
         })
+        State.listData = [...listData]
     }
+
+    static renderPhotoList() {
+        console.log(State.listData)
+        const array = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+        array.forEach(item => {
+            console.log(item)
+            const listBoxItem = {...State.listData[item]}
+            const listEl = document.querySelector(`#list-box-${item}`)
+            listEl.style.background = `url(${listBoxItem.imgUrl}) no-repeat center center`
+            listEl.style.backgroundSize = 'cover'
+        })
+     //  setTimeout(() => {this.clearPhotoList()}, 5000)
+     //  this.renderLevel2()
+    }
+
+    // static renderPhotoList() {
+    //     const randomizedImagesUrls = this.shuffleImages(State.images32)
+    //     const listBoxes = document.querySelectorAll('.list-box')
+    //     listBoxes.forEach((listBox, index) => {
+    //         // const listBoxImage = listBoxes[index].querySelector('img')
+    //         // listBoxImage.src = randomizedImagesUrls[index]
+    //         listBoxes[index].style.background = `url(${randomizedImagesUrls[index]}) no-repeat center center`;
+    //         listBoxes[index].style.backgroundSize = 'cover'
+    //     })
+    //     // this.listDataFunction()
+    //     // setTimeout(() => {this.clearPhotoList()}, 5000)
+    //     // this.renderLevel2()
+    // }
 
     static clearPhotoList() {
         const listBoxes = document.querySelectorAll('.list-box')
